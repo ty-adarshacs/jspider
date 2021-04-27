b036911a4ab025f0cb85e8ce6eb8e710cf0a8334
ty-adarshacs <82380860+ty-adarshacs@users.noreply.github.com>
2021-04-27 19:21:43 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package aaa.jsp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
